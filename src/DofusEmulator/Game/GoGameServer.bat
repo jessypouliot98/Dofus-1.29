f667f2a7ff3f62a2763278aa200eecaf357ed64d
Jessy Pouliot <jessypouliot98@gmail.com>
2022-10-02 00:43:24 -0400

    save

--- src/DofusEmulator/Game/GoGameServer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title ARv54 GameServer
-:loop
-java -jar -Xmx1024m -Xms1024m AncestraGame.jar
-goto loop
-PAUSE
--- src/DofusEmulator/Realm/startRealm.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title ARv54 AuthServer
-:loop
-java -jar -Xmx200m -Xms200m AncestraRealm.jar
-goto loop
-PAUSE
